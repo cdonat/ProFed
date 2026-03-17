26e1e5d66141396ab5f488cdaa120f76803e2c3a
Christof Donat <cd@okunah.de>
2026-03-17 23:09:26 +0100

    adapt content type for actor endpoint to ActivityPub standard

--- src/profed/components/api/routers/actor.py
+++ src/profed/components/api/routers/actor.py
@@ -2,12 +2,21 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from fastapi import APIRouter, HTTPException, Path
+from fastapi.responses import JSONResponse
 from profed.components.api.services.actor import resolve_actor
 from profed.components.api.models.actor import Actor
 
+
 router = APIRouter()
 
-@router.get("/actors/{username}", response_model=Actor)
+
+class ActivityPubJSONResponse(JSONResponse):
+    media_type = "application/activity+json"
+
+
+@router.get("/actors/{username}",
+            response_model=Actor,
+            response_class=ActivityPubJSONResponse)
 async def actor(username: str = Path(pattern=r"^[a-zA-Z0-9_.-]+$")):
     try:
         actor = await resolve_actor(username)
